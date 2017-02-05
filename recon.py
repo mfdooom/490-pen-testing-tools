27e29bff1ee150146bcecb551cbcec917295ad81
Shels1909 <knightm@localhost.localdomain>
2017-02-05 15:30:57 -0500

    DNS and IP recon now work succesfully

--- recon.py
+++ recon.py
@@ -1,9 +1,10 @@
+import string
 import subprocess 
 
 def get_DNS_servers(website):
-    subprocess.Popen("whois %s >> temp.txt" % website, shell=True)
+    subprocess.call("whois %s > temp.txt" % website, shell=True)
 
-    with open("temp.txt") as f:
+    with open("temp.txt", 'r') as f:	
         content = f.readlines()
 
     dns_servers = []
@@ -15,28 +16,51 @@ def get_DNS_servers(website):
     # remove duplicates from dns_servers
     dns_servers = list(set(dns_servers))
     
-    # remove temp.txt file
-    subproces.Popen("rm temp.txt", shell=True)
     
+    # create dns_servers txt file and populate with dns info	
+    subprocess.Popen("touch dns_servers.txt", shell=True)
     i = 0
     for dns_server in dns_servers:
         if i == 0:
-            subprocess.Popen("echo DNS recon info on %s > dns_servers.txt"  % website, shell=True);
+            subprocess.Popen("echo DNS recon info on %s > dns_servers.txt" % website, shell=True);
             subprocess.Popen("echo %s >> dns_servers.txt" % dns_server, shell=True)
         else:
             subprocess.Popen("echo %s >> dns_servers.txt" % dns_server, shell=True)
         i = i + 1
 
-def get_IP_addresses(website):
-    subprocess.Popen("host %s > temp.txt" % website, shell=True)
-    
-    # get the first line from the host command
-    with  open('temp.txt', 'r') as f:       
-        first_line  = f.readline()
+    # remove temp.txt file
+    subprocess.Popen("rm temp.txt", shell=True)
 
-    subprocess.Popen("echo %s > IPs.txt" % first_line, shell=True)
+def get_IP_addresses(website):
+
+    # get the IP address for the website	
+    subprocess.call("host %s > temp.txt" % website, shell=True)
+    with open("temp.txt", 'r') as f:       
+        first_line  = f.readline().strip()
+
+    subprocess.call("echo %s IP address: > IPs.txt" % website, shell=True)
+    subprocess.call("echo %s >> IPs.txt" % first_line, shell=True)
+
+    # get the IP addresses for the websites DNS servers
+    subprocess.call("whois %s > dns_temp.txt" % website, shell=True)
+    with open("dns_temp.txt", 'r') as f:	
+        content = f.readlines()
+
+    dns_servers = []
+    for line in content:
+        if "Name Server" in line:
+            line = line.strip()
+            dns_servers.append(line)
+
+    # remove duplicates from dns_servers
+    dns_servers = list(set(dns_servers))
     
-        
+    subprocess.call("echo %s DNS server IP addresses: >> IPs.txt" % website, shell=True)
+    for dns_server in dns_servers:
+	dns_server_name = string.replace(dns_server, "Name Server: ", "")
+        subprocess.call("host %s >> IPs.txt" % dns_server_name, shell=True) 
+    
+    subprocess.call("rm temp.txt dns_temp.txt", shell=True)
 
 def get_recon_method():
     recon_method = raw_input("Select one of the folloing reconnaisance options: \n1)IP Addresses\n2)DNS Servers\n3)Public Files\n")
@@ -46,6 +70,7 @@ def main():
     website = raw_input("Enter the name of the site you wish to perform reconnaisance on:")
     
     recon_method = get_recon_method()
+
     if recon_method == "1":
         get_IP_addresses(website)
     elif recon_method == "2":
@@ -55,6 +80,7 @@ def main():
         get_public_files(website)
     else: 
         get_recon_method()
+    
 
 
 main()
