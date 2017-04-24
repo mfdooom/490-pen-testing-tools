d32f0e032e849d6d1782afcde815e65335ad6af3
Mac Knight <mknight1996@gmail.com>
2017-04-23 20:32:45 -0400

    Script now outputs the captured password data in a nice readable format for user of script

--- lockPacketFilter.py
+++ lockPacketFilter.py
@@ -6,10 +6,10 @@ desired_handle = "0x0000002d"
 desired_serviceUUID = "65488"
 desired_uuid = "65494"
 
-with open("lockPacketInfo.json") as data_file:
+with open("lockPackets/lockPacketInfo.json") as data_file:
     data = json.load(data_file)
 
-
+btattValues = []
 for dataPoint in data:
     try:
         if (dataPoint["_source"]["layers"]["btatt"]["btatt.handle"] ==
@@ -21,11 +21,24 @@ for dataPoint in data:
 
             #print("######FOUND IT########")
             #pprint(dataPoint["_source"]["layers"]["frame"]["frame.number"])
-            pprint(dataPoint["_source"]["layers"]["btatt"]["btatt.value"])
+            btattValue = dataPoint["_source"]["layers"]["btatt"]["btatt.value"] 
+            btattValues.append(btattValue)
+
     except Exception as e:
         pass
         #print(e)
     
 
+if not btattValues:
+    print "No Password Captured"
+else:
+    for btattValue in btattValues:
+        btattValue = str(btattValue)
+        opcode = btattValue.split(':')[0]
+        oldPassword = "".join(btattValue.split(':')[1:4])
+        newPassword = "".join(btattValue.split(':')[5:8])
 
-
+        if opcode == "01":
+            print "Password Change Captured!\nOpcode: " + opcode +  "\nOld Password: " + oldPassword + "\nNew Password: " + newPassword + "\n"
+        elif opcode == "00":
+            print "Password Captured!\nOpcode: " + opcode +  "\nPassword: " + newPassword + "\n"
